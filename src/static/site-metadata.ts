b8ec9164cf5edcba8c12b97ee7ca9e679a95c351
ylinwind <ylinwind@gmail.com>
2024-12-24 11:48:27 +0800

    .

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'Running Ylin',
+  siteUrl: 'https://ylinwind.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Ylinwind Running Page',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://github.com/ylinwind',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/ylinwind/running_page/blob/master/README-CN.md',
     },
   ],
 };
